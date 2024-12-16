7fdf9addf7881000eb7c75ffe52e7831542dac36
tgvarik <tgvarik@users.noreply.github.com>
2024-12-16 15:53:29 -0800

    [hdPrman] Fix usage of disableJitter
    This change includes a fix to how the environment variable HD_PRMAN_DISABLE_HIDER_JITTER is interpreted. Previously, leaving this environment variable unset forced hider:jitter to true, overriding any value set in RenderSettings. Now it will force the setting only if it is set to a truthy value. If left unset or set to false, it has no effect, and any value for hider:jitter authored via RenderSettings will be respected.
    
    (Internal change: 2351627)

--- third_party/renderman-26/plugin/hdPrman/utils.cpp
+++ third_party/renderman-26/plugin/hdPrman/utils.cpp
@@ -79,16 +79,16 @@ struct _VtValueToRtParamList
     // Gf types
     //
     bool operator()(const GfVec2i &v) {
-        return params->SetIntegerArray(name, v.data(), 2); 
+        return params->SetIntegerArray(name, v.data(), 2);
     }
     bool operator()(const GfVec2f &v) {
-        return params->SetFloatArray(name, v.data(), 2); 
+        return params->SetFloatArray(name, v.data(), 2);
     }
     bool operator()(const GfVec2d &vd) {
         return (*this)(GfVec2f(vd));
     }
     bool operator()(const GfVec3i &v) {
-        return params->SetIntegerArray(name, v.data(), 3); 
+        return params->SetIntegerArray(name, v.data(), 3);
     }
     bool operator()(const GfVec3f &v) {
         if (role == HdPrimvarRoleTokens->color) {
@@ -107,7 +107,7 @@ struct _VtValueToRtParamList
         return (*this)(GfVec3f(vd));
     }
     bool operator()(const GfVec4i &v) {
-        return params->SetIntegerArray(name, v.data(), 4); 
+        return params->SetIntegerArray(name, v.data(), 4);
     }
     bool operator()(const GfVec4f &v) {
         return params->SetFloatArray(name, v.data(), 4);
@@ -160,7 +160,7 @@ struct _VtValueToRtParamList
     // Arrays of Gf types
     //
     bool operator()(const VtArray<GfVec2f> &v) {
-        return params->SetFloatArray(name,   
+        return params->SetFloatArray(name,
             reinterpret_cast<const float*>(v.cdata()), 2*v.size());
     }
     bool operator()(const VtArray<GfVec2d> &vd) {
@@ -243,7 +243,7 @@ struct _VtValueToRtParamList
             assetPath, flipTexture, writeAsset, _tokens->primvar.GetText());
         return params->SetString(name, v);
     }
-    
+
     //
     // Arrays of string-like types
     //
@@ -461,7 +461,7 @@ struct _VtValueToRtPrimVar : _VtValueToRtParamList
     bool operator()(const VtArray<SdfAssetPath> &v) {
         // Convert to RtUString.
         // Since we can't know how the texture will be consumed,
-        // go with the default of flipping textures 
+        // go with the default of flipping textures
         // and that it doesn't want to be written to disk.
         const bool flipTexture = true;
         const bool writeAsset = false;
@@ -694,7 +694,7 @@ ResolveAssetToRtUString(
     // to RtxHioImage (or another Rtx plugin) for this.
     // FUTURE NOTE: When we want to support primvar substitutions with
     // the use of non-tex textures, the following clause can no longer
-    // be an "else if" (because such paths won't ArResolve), and we may 
+    // be an "else if" (because such paths won't ArResolve), and we may
     // not be able to even do an extension check...
     else if (!writeAsset && !_IsNativeRenderManFormat(v) &&
              imageRegistry.IsSupportedImageFile(v)) {
@@ -703,7 +703,7 @@ ResolveAssetToRtUString(
     }
 
     TF_DEBUG(HDPRMAN_IMAGE_ASSET_RESOLVE)
-        .Msg("Resolved %s asset path: %s\n", 
+        .Msg("Resolved %s asset path: %s\n",
              debugNodeType ? debugNodeType : "image",
              v.c_str());
 
@@ -716,8 +716,8 @@ PruneDeprecatedOptions(
 {
     // The following should not be given to Riley::SetOptions() anymore.
     static std::vector<RtUString> const _deprecatedRileyOptions = {
-        RixStr.k_Ri_PixelFilterName, 
-        RixStr.k_hider_pixelfiltermode, 
+        RixStr.k_Ri_PixelFilterName,
+        RixStr.k_hider_pixelfiltermode,
         RixStr.k_Ri_PixelFilterWidth,
         RixStr.k_Ri_ScreenWindow};
 
@@ -794,7 +794,7 @@ GetDefaultRileyOptions()
     options.SetFloat(RixStr.k_Ri_FormatPixelAspectRatio, 1.0f);
     options.SetFloat(RixStr.k_Ri_PixelVariance, 0.001f);
     options.SetString(RixStr.k_bucket_order, RtUString("circle"));
-    
+
     float shutterInterval[2] = {
         HDPRMAN_SHUTTEROPEN_DEFAULT,
         HDPRMAN_SHUTTERCLOSE_DEFAULT
@@ -826,7 +826,9 @@ GetRileyOptionsFromEnvironment()
     }
 
     const bool disableJitter = TfGetEnvSetting(HD_PRMAN_DISABLE_HIDER_JITTER);
-    options.SetInteger(RixStr.k_hider_jitter, !disableJitter);
+    if (disableJitter) {
+        options.SetInteger(RixStr.k_hider_jitter, !disableJitter);
+    }
 
     if (ArchHasEnv("HD_PRMAN_MAX_SAMPLES")) {
         const int maxSamples = TfGetenvInt("HD_PRMAN_MAX_SAMPLES", 64);
